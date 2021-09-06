9c208d26718cb343037a82fb260275496ee9eae7
Ciscodefran <jellycisko@gmail.com>
2021-09-06 15:12:25 +0900

    fix

--- front/src/views/akinator/Arkinator.vue
+++ front/src/views/akinator/Arkinator.vue
@@ -202,6 +202,15 @@ export default {
               this.akinatorLoaded = true;
               this.foodDataLoaded = true;
               this.setStage();
+              if (this.isFirst == true) {
+                let firstRandChoice =
+                  this.alternatives[
+                    Math.floor(Math.random() * this.alternatives.length)
+                  ];
+                this.choiceAkinator(firstRandChoice);
+
+                this.isFirst = false;
+              }
             }
           });
         }
@@ -341,6 +350,7 @@ export default {
       akinatorMeta: [], // 아키네이터 빅데이터
       foodFilter: "", // 검색창
       selectedFood: null, // 선택한 음식정보
+      isFirst: true,
     };
   },
   mounted() {
