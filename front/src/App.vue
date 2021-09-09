782bdfc1ac537677667d2621b3709175fdfe479e
wlsglckals@gmail.com <wlsglckals@gmail.com>
2021-09-09 13:10:12 +0900

    checked

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <Sidebar />
     <transition name="page">
-      <div class="col-md-12" :style="{ 'margin-left': sidebarWidth }" style="margin: 0 auto;">
+      <div class="col-md-12" :style="{ 'margin-left': sidebarWidth }">
         <router-view />
       </div>
     </transition>
